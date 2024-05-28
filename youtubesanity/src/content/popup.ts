cf43fa89165ad55d04ad2a69adb39466791ffe84
Purav <puravparabdev@gmail.com>
2024-05-28 14:03:36 -0700

    updated popup to accept inputs

--- youtubesanity/src/content/popup.ts
+++ youtubesanity/src/content/popup.ts
@@ -1,16 +1,43 @@
-document.getElementById('RunBtn')?.addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tabId = tabs[0]?.id;
-    if (tabId !== undefined) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ["content.js"]
-      }, () => {
-        chrome.tabs.sendMessage(tabId, { action: "runScraper" }, (response) => {
-        });
-      });
-    } else {
-      console.error('Tab Id not found.');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+	const promptInput = document.getElementById('prompt') as HTMLInputElement;
+	const modelInput = document.getElementById('model') as HTMLInputElement;
+	const apiKeyInput = document.getElementById('apiKey') as HTMLInputElement;
+	const runButton = document.getElementById('RunBtn') as HTMLInputElement;
+	console.log(promptInput, modelInput, apiKeyInput, runButton);
+
+	// Load stored values from localStorage
+	chrome.storage.local.get(['prompt', 'model', 'apiKey'], (result) => {
+  	if (result.prompt) {promptInput.value = result.prompt;}
+    if (result.model) {modelInput.value = result.model;}
+    if (result.apiKey) {apiKeyInput.value = result.apiKey;}
   });
-});
+
+	// Store inputs to local storage
+	const storeInputs = () => {
+		const data = {
+      prompt: promptInput.value,
+      model: modelInput.value,
+      apiKey: apiKeyInput.value
+    };
+    chrome.storage.local.set(data, () => {
+      console.log('Data stored:', data);
+    });
+	};
+
+	document.getElementById('RunBtn')?.addEventListener('click', () => {
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			const tabId = tabs[0]?.id;
+			if (tabId !== undefined) {
+				chrome.scripting.executeScript({
+					target: { tabId: tabId },
+					files: ["content.js"]
+				}, () => {
+					storeInputs();
+					chrome.tabs.sendMessage(tabId, { action: "runScraper" }, (response) => {});
+				});
+			} else {
+				console.error('Tab Id not found.');
+			}
+		});
+	});
+})
--- youtubesanity/src/utilities/classify.ts
+++ youtubesanity/src/utilities/classify.ts
@@ -2,6 +2,8 @@ import { Video } from './interfaces';
 
 const classify = async (videoList: Video[]) : Promise<Video[]> => {
 	// console.log("classify", videoList);
+	// const model = await chrome.storage.local.get('model');
+	// console.log(model);
 	return videoList;
 };
 
